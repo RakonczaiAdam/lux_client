06ad51b9520be3f795581e72e65efb6e187e428f
Zoltan Reviczky <reviczkyzoli@gmail.com>
2020-12-21 13:26:08 +0100

    router changes

--- src/components/router.jsx
+++ src/components/router.jsx
@@ -2,18 +2,10 @@ import React from "react";
 import Login from "./login/login";
 import Register from "./login/register";
 import Feed from "./dashboard/feedscroll";
-import Profile from "./dashboard/profile";
-import People from "./dashboard/people";
-import Settings from "./dashboard/settings";
-import Notifications from "./dashboard/notifications";
 
 const routes = {
     "/login": () => <Login />,
     "/register": () => <Register />,
     "/feed": () => <Feed />,
-    "/profile": () => <Profile />,
-    "/people": () => <People />,
-    "/settings": () => <Settings />,
-    "/notifications": () => <Notifications />
   };
 export default routes;
